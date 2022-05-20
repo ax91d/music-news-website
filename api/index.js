79b3d17b15e1464b3e0408137e6e30da24cedb45
ax91d <moonwisher182@gmail.com>
2022-05-20 22:59:04 +0200

    add back end

---
+++ api/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+
+app.use(express.json());
+
+mongoose
+  .connect("mongodb://localhost:27017/musicWebsite")
+  .then(() => {
+    console.log("Connection to MongoDB open!");
+  })
+  .catch((err) => {
+    console.log("Oh no, error!");
+    console.log(err);
+  });
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "images");
+  },
+  filename: (req, file, callback) => {
+    callback(null, "hello.jpeg");
+  },
+});
+
+const upload = multer({ storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
+app.listen("5000", () => {
+  console.log("PORT 5000, Up and running...");
+});
