e5a093ee3a3684bfff5c71131e6e1490228a60b7
ax91d <moonwisher182@gmail.com>
2022-05-23 14:06:24 +0200

    add footer and 2 pages About and Contact

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import TopBar from "./components/topbar/TopBar";
+import Footer from "./components/footer/Footer";
 import Register from "./pages/register/Register";
 import Login from "./pages/login/Login";
 import Settings from "./pages/settings/Settings";
 import Write from "./pages/write/Write";
 import Home from "./pages/home/Home";
+import Contact from "./pages/contact/Contact";
+import About from "./pages/about/About";
 import Single from "./pages/single/Single";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { useContext } from "react";
@@ -13,6 +16,8 @@ function App() {
   const {user} = useContext(Context);
   return (
     <Router>
+    <div style={{minHeight: "100vh",display: "flex",
+    flexDirection: "column"}}>
       <TopBar />
       <Switch>
         <Route exact path="/">
@@ -22,10 +27,15 @@ function App() {
         <Route path="/login">{user ? <Home /> : <Login />}</Route>
         <Route path="/write">{user ? <Write /> : <Login />}</Route>
         <Route path="/settings">{user ? <Settings /> : <Login />}</Route>
+        <Route path="/contact"><Contact/></Route>
+        <Route path="/about"><About/></Route>
         <Route path="/post/:postId">
           <Single />
         </Route>
       </Switch>
+      <Footer/>
+      </div>
+
     </Router>
   );
 }
---
+++ client/src/pages/about/About.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./about.css";
+
+export default function About() {
+  return (
+    <div className="about-wrap">
+      <div className="about-heading">
+        {" "}
+        <b>About</b>{" "}
+      </div>
+      <p className="about-text">
+        This is a media resource about the music industry and culture. A
+        music enthusiast has designed and engineered this project, which reveals the
+        genres of popular music for the listener. My goal is to bring together
+        a community of people interested in all things music and highlight
+        important industry events. Here I talk about all musical genres in an
+        accessible language. Here you can find out not only the latest news, but
+        also understand the history of genres and look at familiar artists from
+        a new angle.
+      </p>
+    </div>
+  );
+}
