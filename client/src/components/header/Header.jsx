974cb91c87dc0e2b050ad462d0348133f6fb7d70
ax91d <moonwisher182@gmail.com>
2022-05-23 19:45:26 +0200

    add categories

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,4 +1,5 @@
 import "./header.css";
+import bootstrap from "bootstrap";
 
 export default function Header() {
   return (
@@ -7,11 +8,14 @@ export default function Header() {
         <span className="headerTitleSm">Akhmet Dossanov</span>
         <span className="headerTitleLg">Strings & Rhymes</span>
       </div>
+      
       <img
         className="headerImg"
         src="https://guitar.com/wp-content/uploads/2021/10/Angels-Airwaves-Credit-Kevin-Trout-9@2160x1215.jpg"
         alt=""
       />
+      
+      
     </div>
   );
 }
